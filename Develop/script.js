ef1d506e5aa0715c397852370958af0810e3829e
Charles Molitor <Charlie@Charles-MacBook-Pro.local>
2022-06-30 17:52:27 -0700

    added arrays of all criteria including numeric, lower, upper, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -9,19 +9,22 @@ function generatePassword() {
   var lowerCase = confirm('Would you like to include lower case characters?');
     if (lowerCase) {
       //include lower case characters
+      lowerCaseCharacterArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     }
   var upperCase = confirm('Would you like to include upper case characters?');
     if (upperCase) {
       //include upper case characters
+      upperCaseCharacterArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
     }
   var numeric = confirm('Would you like to include numberic characters?');
     if (numeric) {
       //include numeric characters
+      numericCharacterArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     }
   var specialCharacters = confirm('Would you like to include special characters?');
     if (specialCharacters) {
       //include special characters
-      specialCharacterArray = "!@#$%^&*()?><[]{}=+`~";
+      specialCharacterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '<', '>', '/', '?', '~', '`'];
     }
   
 }
