2b1b5475fa78198e5a5acc5b969bcf6f94ec068b
Charles Molitor <Charlie@Charles-MacBook-Pro.local>
2022-06-30 17:46:08 -0700

    Added prompts to all criteria of password and started working on including all possible characters in arrays for now for future password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,30 @@
 // Assignment code here
-
+function generatePassword() {
+  var firstPrompt = alert('Welcome to the random password generator, please answer the following prompts to customize your new password');
+  var passwordLength = prompt("Please enter how many characters you would like your password from 8-128");
+    while (passwordLength < 8 || passwordLength > 128) {
+      alert("You may only have a password character length between 8-128");
+      var passwordLength = prompt("Please enter how many characters you would like your password from 8-128");
+    }
+  var lowerCase = confirm('Would you like to include lower case characters?');
+    if (lowerCase) {
+      //include lower case characters
+    }
+  var upperCase = confirm('Would you like to include upper case characters?');
+    if (upperCase) {
+      //include upper case characters
+    }
+  var numeric = confirm('Would you like to include numberic characters?');
+    if (numeric) {
+      //include numeric characters
+    }
+  var specialCharacters = confirm('Would you like to include special characters?');
+    if (specialCharacters) {
+      //include special characters
+      specialCharacterArray = "!@#$%^&*()?><[]{}=+`~";
+    }
+  
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +40,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
